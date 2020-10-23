21f081625b4046143fd6eff0753b1888f802fc08
Mario Santoro <43414100+mario-santoro@users.noreply.github.com>
2020-10-23 11:56:00 +0200

    Update MySQL.cs

--- MySQL.cs
+++ MySQL.cs
@@ -22,8 +22,8 @@ namespace FaceUnlockVocalNode
         {
             SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
             builder.DataSource = "server-faccia.database.windows.net";
-            builder.UserID = "annunziata";
-            builder.Password = "mario-94";
+            builder.UserID = "XXXXXXXXXX";
+            builder.Password = "XXXXXXXXXX";
             builder.InitialCatalog = "app";
 
             return builder;
